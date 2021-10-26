3ff318dc84a4368037b82ec609277f848164f629
work <serj2107@gmail.com>
2021-10-26 19:02:35 +0300

    Setting project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Анкета соискателя</title>
+      </Head>
+
+      <h1>Home page</h1>
+    </>
+  );
+};
+
+export default Home;
